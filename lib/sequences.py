98c23368b0cbf3f381b999bed3219705e41e84b5
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-11 12:31:26 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    numbers = []
+    for i in range(0, length):
+        if (i < 2):
+            numbers.append(i)
+        else:
+            numbers.append(numbers[i-1] + numbers[i-2])
+        
+    print(numbers)
+
+
+(print_fibonacci(10))
